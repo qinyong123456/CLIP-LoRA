b485a1b8fcc79be3fca6c193feeebf404ccb582a
PedroPiana <pedroaspiana@gmail.com>
2025-07-08 15:04:20 -0400

    test

--- teste.sh
+++ teste.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=Moleteste.out
 #SBATCH --error=Moleteste.err
 #SBATCH --mem=32G
-#SBATCH --time=00:40:00
+#SBATCH --time=00:20:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node=1
@@ -20,7 +20,7 @@ PYTHONWARNINGS="ignore" python3 main.py \
 --dataset oxford_pets \
 --seed 1 \
 --shots 1 \
---num_experts 4 \
+--num_experts 2 \
 --topk 2 \
 --lambda_balance 0.1 \
 --save_path weights \
